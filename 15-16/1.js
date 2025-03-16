6dced984d1177b131c09627126193f658fe79a4e
Arjzona <151389775+Arjzona@users.noreply.github.com>
2025-03-16 20:43:23 +0300

    Add files via upload

---
+++ 15-16/1.js
@@ -0,0 +1,10 @@
+class Alien {
+    constructor (name, phrase) {
+        this.name = name
+        this.species = "alien"
+    }
+
+    fly = () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+    sayPhrase = () => console.log("I can't fly now")
+}
+console.log(alien1.name) 
